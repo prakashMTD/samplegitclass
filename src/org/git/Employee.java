cf2ad82db88070778a2faba12637c9082b251349
prakashMTD <prakashmt95@gmail.com>
2022-08-25 14:47:07 +0530

    First Push code

---
+++ src/org/git/Employee.java
@@ -0,0 +1,36 @@
+package org.git;
+
+public class Employee {
+
+	
+	public void test1() {
+		System.out.println("test1");
+
+	}
+	public void test2() {
+		System.out.println("test2");
+
+	}
+	public void test3() {
+		System.out.println("test3");
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
